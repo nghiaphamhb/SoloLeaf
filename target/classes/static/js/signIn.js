dae619b82a99217feebf695f0f627e3795479321
second <phamdangtrungnghia@gmail.com>
2025-10-22 22:48:47 +0300

    feat: add func sign in

---
+++ target/classes/static/js/signIn.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $("#form-signin").on("submit", function (e) {
+        e.preventDefault();
+        var username = $("#username").val();
+        var password = $("#password").val();
+
+        $.ajax({
+            method: "POST",
+            url: "/api/login/signIn",
+            data: {
+                username: username,
+                password: password
+            }
+        })
+            .done(function (msg) {
+                if (msg && msg.data) {
+                    console.log("[Client] Get token: " + msg.data);
+                    localStorage.setItem("token", msg.data);
+                    window.location.href = "/main";
+                } else {
+                    alert((msg && msg.message) || "Đăng nhập thất bại!");
+                }
+            })
+            .fail(function (xhr) {
+                alert("Lỗi mạng/Server: " + (xhr.status || "") + " " + (xhr.responseText || ""));
+            });
+    });
+
+})
