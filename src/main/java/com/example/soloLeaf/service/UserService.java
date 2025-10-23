af801aa9dcf0cda39658b72f3c5eb8be35ef4f76
second <phamdangtrungnghia@gmail.com>
2025-10-23 03:25:51 +0300

    feat: add services

--- src/main/java/com/example/soloLeaf/service/UserService.java
+++ src/main/java/com/example/soloLeaf/service/UserService.java
@@ -5,6 +5,11 @@ import com.example.soloLeaf.entity.Users;
 import com.example.soloLeaf.repository.UserRepository;
 import com.example.soloLeaf.service.imp.UserServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -30,5 +35,28 @@ public class UserService implements UserServiceImp {
             listUserDTO.add(userDTO);
         }
         return listUserDTO;
+    }
+
+    /** Lấy entity Users hiện tại dựa vào principal đã set trong filter */
+    public Users getCurrentUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null || !(auth.getPrincipal() instanceof UserDetails ud)) {
+            throw new AccessDeniedException("Unauthenticated");
+        }
+        //  principal là username
+        String username = ud.getUsername();
+        try{
+            return userRepository.findByUsername(username);
+        } catch (UsernameNotFoundException e) {
+            throw new UsernameNotFoundException("User not found");
+        }
+    }
+
+    public UserDTO getMyProfile() {
+        Users currentUser = getCurrentUser();
+        return new UserDTO(currentUser.getId(),
+                currentUser.getUsername(),
+                currentUser.getFullname(),
+                currentUser.getCreateDate());
     }
 }
