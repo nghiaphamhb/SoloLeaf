8ac1d5314c6f2d416bf04ae564c4beffa7f6cf4a
second <phamdangtrungnghia@gmail.com>
2025-10-23 03:24:58 +0300

    Update CustomJwtFilter.java

--- src/main/java/com/example/soloLeaf/security/CustomJwtFilter.java
+++ src/main/java/com/example/soloLeaf/security/CustomJwtFilter.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -15,27 +16,41 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * Đây là custom filter trước filter UsernamePasswordAuthentication
+ */
 @Component
 public class CustomJwtFilter extends OncePerRequestFilter {
     @Autowired
     JwtUtilsHelper jwtUtilsHelper;
 
+    @Autowired
+    CustomUserDetailService userDetailsService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = getTokenFromHeader(request);
-        System.out.println("[Server] From request (header) get token: " + token);
+        System.out.println("[Server] From request (header) get token: " + token); // ẩn đi sau khi dự án hoàn thiện
+
         if (token != null) {
             if (jwtUtilsHelper.verifyJwtToken(token)) {
-                //cài token xác thực thông qua cho UsernamePasswordAuthentication - filter ngay sau CustomJwtFilter
-                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken("", "", new ArrayList<>());
-                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                String subject = jwtUtilsHelper.getSubject(token);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(subject); // lấy subject là username
+
+                UsernamePasswordAuthenticationToken authenticationToken = // tạo token cho filter sau
+                        new UsernamePasswordAuthenticationToken(userDetails, null, new ArrayList<>()); // mục thứ 3 là roles
+
+                SecurityContextHolder.getContext()
+                        .setAuthentication(authenticationToken); // set up filter sau vào Security Context
             }
 
         }
-        filterChain.doFilter(request, response);
+        filterChain.doFilter(request, response);// chuyển tiếp request response cho filter sau
     }
 
-
+    /**
+     * Lấy token từ http header request
+     */
     private String getTokenFromHeader(HttpServletRequest request) {
         String headerContainsToken = request.getHeader("Authorization");
         String token = null;
