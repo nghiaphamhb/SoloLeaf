e081dafc601f5edb92960e102f2b20fd9d74dab9
second <phamdangtrungnghia@gmail.com>
2025-10-23 03:25:33 +0300

    feat: add features

--- src/main/java/com/example/soloLeaf/service/imp/UserServiceImp.java
+++ src/main/java/com/example/soloLeaf/service/imp/UserServiceImp.java
@@ -1,9 +1,17 @@
 package com.example.soloLeaf.service.imp;
 
 import com.example.soloLeaf.dto.UserDTO;
+import com.example.soloLeaf.entity.Users;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.List;
 
 public interface UserServiceImp {
     List<UserDTO> getAllUser();
+    Users getCurrentUser();
+    UserDTO getMyProfile();
 }
