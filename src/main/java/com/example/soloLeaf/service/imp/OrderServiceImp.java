4141c91bf7277230c1d9cef52555485d01a48ab0
second <phamdangtrungnghia@gmail.com>
2025-12-20 19:05:29 +0300

    refactor: cleaning the root directory

--- src/main/java/com/example/soloLeaf/service/imp/OrderServiceImp.java
+++
@@ -1,7 +0,0 @@
-package com.example.soloLeaf.service.imp;
-
-import com.example.soloLeaf.payload.request.OrderRequest;
-
-public interface OrderServiceImp {
-    boolean insertOrder(OrderRequest orderRequest);
-}
--- src/main/java/com/example/soloLeaf/service/imp/PromoServiceImp.java
+++
@@ -1,9 +0,0 @@
-package com.example.soloLeaf.service.imp;
-
-import com.example.soloLeaf.dto.PromoDTO;
-
-import java.util.List;
-
-public interface PromoServiceImp {
-    List<PromoDTO> getAllPromo();
-}
