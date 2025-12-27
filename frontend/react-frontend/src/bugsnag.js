75f6e9d4211603fbf97f329e18d1addbe73483c9
second <phamdangtrungnghia@gmail.com>
2025-12-27 07:57:40 +0300

    Update bugsnag.js

--- frontend/react-frontend/src/bugsnag.js
+++ frontend/react-frontend/src/bugsnag.js
@@ -1,12 +1,18 @@
-// src/bugsnag.js
 import Bugsnag from "@bugsnag/js";
 import BugsnagPluginReact from "@bugsnag/plugin-react";
 
-Bugsnag.start({
-  apiKey: import.meta.env.VITE_BUGSNAG_API_KEY,
-  plugins: [new BugsnagPluginReact()],
-  appVersion: "1.0.0",
-  releaseStage: import.meta.env.MODE, // development / production
-});
+const apiKey = import.meta.env.VITE_BUGSNAG_API_KEY;
+
+// Optional: turn off Bugsnag in Cypress/e2e
+const isCypress = Boolean(window.Cypress);
+
+if (apiKey && !isCypress) {
+  Bugsnag.start({
+    apiKey,
+    plugins: [new BugsnagPluginReact()],
+  });
+} else {
+  console.info("Bugsnag disabled (missing key or running Cypress).");
+}
 
 export default Bugsnag;
